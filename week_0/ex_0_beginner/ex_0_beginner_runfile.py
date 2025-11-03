8ca1fd73cae0e85c6c2bd995c9a43e2b4a2e9c29
annemnpinheiro <annemnascimento2@gmail.com>
2025-11-03 20:49:46 -0300

    completed task 1

--- week_0/ex_0_beginner/ex_0_beginner_runfile.py
+++ week_0/ex_0_beginner/ex_0_beginner_runfile.py
@@ -1,3 +1,5 @@
-print("\n" + "My name is Anne Pinheiro, I'm brazilian and I hope in the future I can be successful in my career choice, I hope this python classes will help me to achieve my goals")
+import this
 
-print("\n" + "The last time I was in a place without computers was about 6 months ago in a cute place in the countryside of brazil")
+print("\n"+"my name is Anne Pinheiro, I'm brazilian and I hope in the future I can succeed and I hope this python classes can help me achieve my goals")
+
+print("\n"+"the last time I was in a place without computers was about 6 months ago in the countryside of brazil")
