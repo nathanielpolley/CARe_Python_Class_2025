bfe0fb90ad4d7128a0bb72a82ccf405c0801f24a
ChunLiChung <a.bc96384@gmail.com>
2025-11-04 19:15:15 +0100

    calculation formula finished! 2nd version

--- week_0/ex_0_advanced/ex_0_advanced_runfile.py
+++ week_0/ex_0_advanced/ex_0_advanced_runfile.py
@@ -1,1 +1,11 @@
 #YOUR CODE FOR EX_0 ADVANCED HERE
+x = str(input("Enter your DNA sequence: ")).upper()
+
+count_A = x.count("A")
+count_T = x.count("T")
+count_G = x.count("G")
+count_C = x.count("C")
+count_GCcontent = (x.count("G") + x.count("C"))/len(x)
+
+
+print("count_A =", count_A, "count_T =", count_T, "count_G =", count_G, "count_C =", count_C, "GC content = count_GCcontent =", count_GCcontent )
--- week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
+++ week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
@@ -1,8 +1,17 @@
 #YOUR CODE FOR EX_0 INTERMEDIATE HERE
-x = int(input("Enter initial_count"))
-y = int(input("Enter final_count"))
-z = int(input("time_min"))
+import math
+from selectors import SelectSelector
+initial_count = input("Enter initial_count")
+final_count = input("Enter final_count")
+time = input("Enter time")
+if initial_count.isdigit() and final_count.isdigit and time.isdigit:
+    x = int(initial_count)
+    y = int(final_count)
+    z = int(time)
+    Growth_rate = (math.log(y) - math.log(x)) / z
+    print(Growth_rate)
+else:
+    print("ERROR! Go back and check again!")
 
-Growth_rate = ( y - x ) / z
 
-print(Growth_rate)
+
