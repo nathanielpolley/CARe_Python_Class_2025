1e4d3288f0d0ef8056b6b2220026c4e32a7ff67b
ChunLiChung <a.bc96384@gmail.com>
2025-11-06 22:16:12 +0100

    calculation formula finished! 3nd version

--- week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
+++ week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
@@ -1,10 +1,11 @@
 #YOUR CODE FOR EX_0 INTERMEDIATE HERE
 import math
-from selectors import SelectSelector
 initial_count = input("Enter initial_count")
 final_count = input("Enter final_count")
 time = input("Enter time")
-if initial_count.isdigit() and final_count.isdigit and time.isdigit:
+if initial_count > final_count:
+    print("ERROR! Go back and check again!")
+elif initial_count.isdigit() and final_count.isdigit() and time.isdigit():
     x = int(initial_count)
     y = int(final_count)
     z = int(time)
