d9f89e31002cf4ff1bc753a4f5b5026b64fe7ddb
ChunLiChung <a.bc96384@gmail.com>
2025-11-13 20:39:31 +0100

    Hope it works this time :>

--- week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
+++ week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
@@ -3,16 +3,18 @@ import math
 initial_count = input("Enter initial_count")
 final_count = input("Enter final_count")
 time = input("Enter time")
-if initial_count > final_count:
-    print("ERROR! Go back and check again!")
-elif initial_count.isdigit() and final_count.isdigit() and time.isdigit():
+if initial_count.isdigit() and final_count.isdigit() and time.isdigit():
     x = int(initial_count)
     y = int(final_count)
     z = int(time)
-    Growth_rate = (math.log(y) - math.log(x)) / z
-    print(Growth_rate)
+    if x > y:
+        print("Final count should higher than initial count")
+    elif x<= 0 or y <= 0 or z <= 0:
+        print("Error input")
+    else:
+        Growth_rate = (math.log(y) - math.log(x)) / z
+        print(Growth_rate)
 else:
-    print("ERROR! Go back and check again!")
-
+    print ("ERROR! Go back and check again!")
 
 
