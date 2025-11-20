649216cfb9d84071a81a180b1baf3c726f1b7fd1
toquochoa <toquochoa2001@gmail.com>
2025-11-20 07:50:14 +0100

    Update ex_0_advanced_runfile.py

--- week_0/ex_0_advanced/ex_0_advanced_runfile.py
+++ week_0/ex_0_advanced/ex_0_advanced_runfile.py
@@ -1,15 +1,24 @@
 inp=input( "enter DNA sequence: ")
 DNA = inp.upper()
-a = DNA.count("A") 
-c = DNA.count("C") 
-g = DNA.count("G") 
-t = DNA.count("T")
-length = a + c + g + t
-GC = (g+c)/length * 100
-if length == 0:
-    print("No DNA input")
-else:
+inv = 0
+valid = {"A", "T", "G", "C"}
+for Nu in DNA:
+    if Nu not in valid:
+        inv = inv + 1
+        break
+    else:
+        a = DNA.count("A")
+        c = DNA.count("C")
+        g = DNA.count("G")
+        t = DNA.count("T")
+        length = a + c + g + t
+        GC = (g+c)/length * 100
+if inv == 0:
     print(f"length of DNA sequence is : {length}")
     print(f"Number of each nucleotide A C G T respectively:, {a } {c} {g} {t} ")
     print(f"GC content, {GC} %")
+else:
+    print("invalid nucleotide")
+
+
 #YOUR CODE FOR EX_0 ADVANCED HERE
